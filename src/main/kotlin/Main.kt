391d0c7da5f45820e27d9a7fd4008c61c250936f
Mehranj73 <mehranj73@gmail.com>
2022-11-07 16:33:51 +0330

    Calculator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,28 @@
-
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 import org.antlr.v4.runtime.tree.ParseTree
-import org.antlr.v4.runtime.tree.ParseTreeWalker
+
 
 
 fun main(args: Array<String>) {
 
-    val text = "{4, 5}"
+    val text = """
+        4+5
+        x = 2
+        y=5
+        x+y
+        
+    """.trimIndent()
     val input = CharStreams.fromString(text)
 
-    val lexer = ArrayInitLexer(input)
-
+    val lexer = LabeledExprLexer(input)
     val tokens = CommonTokenStream(lexer)
+    val parser = LabeledExprParser(tokens)
+    val tree: ParseTree = parser.prog() // parse
 
-    val parser = ArrayInitParser(tokens)
-    val tree: ParseTree = parser.init() // begin parsing at init rule
-    val walker = ParseTreeWalker()
 
-    walker.walk(ShortToUnicodeString(), tree)
-    println() // print a \n after translation
+    val eval = EvalVisitor()
+    eval.visit(tree)
 
 
 }
